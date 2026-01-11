b674a93c65762343ea558a0b515f57e38438e6b2
Okunichiyou <seifuzi@icloud.com>
2026-01-11 15:53:52 +0900

    メモリを出力するには{:p}と&変数名のセット

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,4 @@
 fn main() {
-    let mut s = "hello".to_string();
-    println!("s= {}", s);
-    clear_s(&mut s);
-    println!("s= {}", s);
-}
-
-fn clear_s(s: &mut String) {
-    s.clear();
+    let x = 1;
+    println!("address: {:p}", &x);
 }
