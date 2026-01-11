811db2386b012b550c0bda7bc9c90ea29d3c0f63
Okunichiyou <seifuzi@icloud.com>
2026-01-11 16:09:09 +0900

    同時参照の禁止はスコープが重ならないことなので、関数使用後に再度同じ変数のリファレンスを作成することができる

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
-    let x = 1;
-    println!("address: {:p}", &x);
+    let mut s = "Hello".to_string();
+
+    let s_ref = &mut s;
+    clear_s(s_ref);
+    println!("s= {}", s);
+
+    let s_ref2 = &mut s;
+    clear_s(s_ref2);
+    println!("s= {}", s);
+}
+
+fn clear_s(s: &mut String) {
+    s.clear();
 }
