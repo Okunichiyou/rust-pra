575dbf1be36f619dc22cf3979899883e8604b9b2
Okunichiyou <seifuzi@icloud.com>
2026-01-11 15:36:23 +0900

    リファレンスを通して変数を借用できる

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     let s = "hello".to_string();
-    print_some(s);
-    print_some(s);
+    print_some(&s);
+    print_some(&s);
 }
 
-fn print_some(s: String) {
-    println!("{}", s)
+fn print_some(s: &String) {
+    println!("{}", *s)
 }
