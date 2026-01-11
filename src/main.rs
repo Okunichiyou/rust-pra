19c1ce865f35c379c4b82d4d279f4cb47ea7893f
Okunichiyou <seifuzi@icloud.com>
2026-01-11 12:35:50 +0900

    モジュール分割

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,5 @@
-fn add_i32(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-#[test]
-fn test1() {
-    assert_eq!(add_i32(1, 2), 3);
-}
-
-#[test]
-fn test2() {
-    assert_eq!(add_i32(5, 9), 14);
-}
+mod module_hello;
 
 fn main() {
-    println!("{}", add_i32(2, 5))
+    module_hello::print_hello();
 }
