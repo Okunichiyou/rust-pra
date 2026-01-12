ee44e0f67498df79648cac8fae7268ab01866056
Okunichiyou <seifuzi@icloud.com>
2026-01-12 10:01:57 +0900

    1つの配列をスライスしたものはリファレンスを返却できる

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 fn main() {
-    let mut s = "Hello".to_string();
-
-    let s_ref = &mut s;
-    clear_s(s_ref);
-    println!("s= {}", s);
-
-    let s_ref2 = &mut s;
-    clear_s(s_ref2);
-    println!("s= {}", s);
+    let v1 = [1, 2, 3, 4, 5];
+    let p = pick1(&v1, 2);
+    for ss in p {
+        println!("{}", ss);
+    }
 }
 
-fn clear_s(s: &mut String) {
-    s.clear();
+fn pick1(x: &[i32], end: usize) -> &[i32] {
+    &x[..end]
 }
